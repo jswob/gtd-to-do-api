f73918ec1c10875f70898f7a80a991e231c68f13
jacek swoboda <jacswob@gmail.com>
2020-01-17 17:49:31 +0100

    add password hashing to user schema via bcrypt

--- lib/gtd_to_do_api/accounts/user.ex
+++ lib/gtd_to_do_api/accounts/user.ex
@@ -15,6 +15,17 @@ defmodule GtdToDoApi.Accounts.User do
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:email, :password, :avatar_url])
+    |> put_password_hash()
     |> validate_required([:email, :password])
+  end
+
+  defp put_password_hash(
+         %Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset
+       ) do
+    change(changeset, %{password: Bcrypt.add_hash(password)})
+  end
+
+  defp put_password_hash(changeset) do
+    changeset
   end
 end
