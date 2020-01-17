936b9e489ff27c9059048b7f7ad83b4366d50732
jacek swoboda <jacswob@gmail.com>
2020-01-17 18:55:10 +0100

    update user_view

--- lib/gtd_to_do_api_web/views/user_view.ex
+++ lib/gtd_to_do_api_web/views/user_view.ex
@@ -2,19 +2,16 @@ defmodule GtdToDoApiWeb.UserView do
   use GtdToDoApiWeb, :view
   alias GtdToDoApiWeb.UserView
 
-  def render("index.json", %{users: users}) do
-    %{data: render_many(users, UserView, "user.json")}
-  end
-
   def render("show.json", %{user: user}) do
     %{data: render_one(user, UserView, "user.json")}
   end
 
   def render("user.json", %{user: user}) do
-    %{id: user.id,
+    %{
+      id: user.id,
       email: user.email,
       password_hash: user.password_hash,
-      password: user.password,
-      avatar_url: user.avatar_url}
+      avatar_url: user.avatar_url
+    }
   end
 end
