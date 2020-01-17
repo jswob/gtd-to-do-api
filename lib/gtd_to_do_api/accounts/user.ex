ce13071fac988e6345b99d28a9a2ff6dd3013d63
jacek swoboda <jacswob@gmail.com>
2020-01-17 16:48:58 +0100

    create user schema and migration file

---
+++ lib/gtd_to_do_api/accounts/user.ex
@@ -0,0 +1,20 @@
+defmodule GtdToDoApi.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :avatar_url, :string
+    field :email, :string
+    field :password, :string, virutal: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :password, :avatar_url])
+    |> validate_required([:email, :password])
+  end
+end
