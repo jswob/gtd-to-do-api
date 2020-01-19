903ad52cff423d7bdd44586621b02c06ddf03ea7
jacek swoboda <jacswob@gmail.com>
2020-01-19 16:36:08 +0100

    create integration tests for authorization

--- test/gtd_to_do_api_web/controllers/user_controller_test.exs
+++ test/gtd_to_do_api_web/controllers/user_controller_test.exs
@@ -19,10 +19,90 @@ defmodule GtdToDoApiWeb.UserControllerTest do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
+  describe "authentication" do
+    setup %{conn: conn} do
+      conn =
+        conn
+        |> bypass_through(GtdToDoApiWeb.Router, :api)
+        |> get("/api/users/1")
+
+      {:ok, %{conn: conn}}
+    end
+
+    test "requires user authentication on :show, :update, :delete actions and :sign_out actions",
+         %{conn: conn} do
+      Enum.each(
+        [
+          get(conn, Routes.user_path(conn, :show, 123)),
+          put(conn, Routes.user_path(conn, :update, 123, %{})),
+          delete(conn, Routes.user_path(conn, :delete, 123)),
+          post(conn, Routes.user_path(conn, :sign_out))
+        ],
+        fn conn ->
+          assert json_response(conn, 401)
+          assert conn.halted
+        end
+      )
+    end
+
+    test "sign_in with correct data returns user and update session with user id", %{conn: conn} do
+      %User{id: id} = user_fixture(@create_attrs)
+
+      conn =
+        post(
+          conn,
+          Routes.user_path(conn, :sign_in, %{
+            email: @create_attrs.email,
+            password: @create_attrs.password
+          })
+        )
+
+      assert conn.assigns.user.id == id
+
+      assert %{"data" => %{"email" => email, "id" => id}} = json_response(conn, 200)
+    end
+
+    test "sign_in with incorrect data delete user id from session and return 401 error message",
+         %{conn: conn} do
+      id = 123
+      put_session(conn, :user_id, id)
+
+      conn =
+        post(
+          conn,
+          Routes.user_path(conn, :sign_in, %{email: "wrong email", password: "wrong password"})
+        )
+
+      assert get_session(conn, :user_id) == nil
+      assert json_response(conn, 401)
+    end
+
+    test "sign_out drop session", %{conn: conn} do
+      user = user_fixture(@create_attrs)
+
+      conn =
+        post(
+          conn,
+          Routes.user_path(conn, :sign_in, %{
+            email: @create_attrs.email,
+            password: @create_attrs.password
+          })
+        )
+
+      assert conn.cookies != %{}
+
+      conn = post(conn, Routes.user_path(conn, :sign_out))
+
+      assert conn.cookies == %{}
+    end
+  end
+
   describe "create user" do
-    test "renders user when data is valid", %{conn: conn} do
+    test "renders user and update session when data is valid", %{conn: conn} do
       conn = post(conn, Routes.user_path(conn, :create), user: @create_attrs)
       assert %{"id" => id} = json_response(conn, 201)["data"]
+
+      assert get_session(conn, :user_id) == id
 
       conn = get(conn, Routes.user_path(conn, :show, id))
 
@@ -80,10 +160,5 @@ defmodule GtdToDoApiWeb.UserControllerTest do
       assert %{assigns: %{message: "Unauthenticated user"}} =
                get(conn, Routes.user_path(conn, :show, user))
     end
-  end
-
-  defp create_user(_) do
-    user = user_fixture()
-    {:ok, user: user}
   end
 end
