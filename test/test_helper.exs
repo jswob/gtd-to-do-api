63ca30005e9d99957bfaf9cbd0c4bffa7d18734c
jacek swoboda <jacswob@gmail.com>
2020-01-20 10:37:00 +0100

    update test helpers with collection_fixture

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -3,12 +3,15 @@ Ecto.Adapters.SQL.Sandbox.mode(GtdToDoApi.Repo, :manual)
 
 defmodule GtdToDoApi.TestHelpers do
   alias GtdToDoApi.Accounts
+  alias GtdToDoApi.Collections
 
   @current_user_attrs %{
     avatar_url: "some avatar_url",
     email: "some email",
     password: "some password"
   }
+
+  @valid_collection_attrs %{color: "some color", name: "some name"}
 
   def user_fixture(attrs \\ %{}) do
     {:ok, user} =
@@ -17,5 +20,13 @@ defmodule GtdToDoApi.TestHelpers do
       |> Accounts.create_user()
 
     user
+  end
+
+  def collection_fixture(%Accounts.User{} = owner, attrs \\ %{}) do
+    attrs = Enum.into(attrs, @valid_collection_attrs)
+
+    {:ok, collection} = Collections.create_collection(owner, attrs)
+
+    collection
   end
 end
