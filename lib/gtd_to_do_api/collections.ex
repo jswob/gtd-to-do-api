0995d65271c05e5c67aaec5a1970cf29d2b02083
jacek swoboda <jacswob@gmail.com>
2020-01-20 10:36:39 +0100

    create collections context

---
+++ lib/gtd_to_do_api/collections.ex
@@ -0,0 +1,112 @@
+defmodule GtdToDoApi.Collections do
+  @moduledoc """
+  The Collections context.
+  """
+
+  import Ecto.Query, warn: false
+  alias GtdToDoApi.Repo
+
+  alias GtdToDoApi.Collections.Collection
+  alias GtdToDoApi.Accounts.User
+
+  @doc """
+  Returns the list of collections.
+
+  ## Examples
+
+      iex> list_collections()
+      [%Collection{}, ...]
+
+  """
+  def list_collections do
+    Repo.all(Collection)
+  end
+
+  def list_users_collections(%User{id: owner_id}) do
+    query = from(c in Collection, where: c.owner_id == ^owner_id)
+
+    Repo.all(query)
+  end
+
+  @doc """
+  Gets a single collection.
+
+  Raises `Ecto.NoResultsError` if the Collection does not exist.
+
+  ## Examples
+
+      iex> get_collection!(123)
+      %Collection{}
+
+      iex> get_collection!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_collection!(id), do: Repo.get!(Collection, id)
+
+  @doc """
+  Creates a collection.
+
+  ## Examples
+
+      iex> create_collection(%{field: value})
+      {:ok, %Collection{}}
+
+      iex> create_collection(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_collection(%User{} = owner, attrs \\ %{}) do
+    %Collection{}
+    |> Collection.changeset(attrs)
+    |> Ecto.Changeset.put_assoc(:owner, owner)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a collection.
+
+  ## Examples
+
+      iex> update_collection(collection, %{field: new_value})
+      {:ok, %Collection{}}
+
+      iex> update_collection(collection, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_collection(%Collection{} = collection, attrs) do
+    collection
+    |> Collection.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a Collection.
+
+  ## Examples
+
+      iex> delete_collection(collection)
+      {:ok, %Collection{}}
+
+      iex> delete_collection(collection)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_collection(%Collection{} = collection) do
+    Repo.delete(collection)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking collection changes.
+
+  ## Examples
+
+      iex> change_collection(collection)
+      %Ecto.Changeset{source: %Collection{}}
+
+  """
+  def change_collection(%Collection{} = collection) do
+    Collection.changeset(collection, %{})
+  end
+end
