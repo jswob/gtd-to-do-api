c3abbeafa6051336a3409409e94d1bffc7bb8fe2
jacek swoboda <jacswob@gmail.com>
2020-01-27 18:57:11 +0100

    Create functions for dealing with subcollections

--- lib/gtd_to_do_api/collections.ex
+++ lib/gtd_to_do_api/collections.ex
@@ -1,23 +1,14 @@
 defmodule GtdToDoApi.Collections do
-  @moduledoc """
-  The Collections context.
-  """
-
   import Ecto.Query, warn: false
   alias GtdToDoApi.Repo
 
+  alias Ecto.Changeset
   alias GtdToDoApi.Collections.Collection
   alias GtdToDoApi.Accounts.User
 
-  @doc """
-  Returns the list of collections.
+  # Collection
+  # //////////////////////////////////////////////////////////////
 
-  ## Examples
-
-      iex> list_collections()
-      [%Collection{}, ...]
-
-  """
   def list_collections do
     Repo.all(Collection)
   end
@@ -28,20 +19,6 @@ defmodule GtdToDoApi.Collections do
     |> Repo.all()
   end
 
-  @doc """
-  Gets a single collection.
-
-  Raises `Ecto.NoResultsError` if the Collection does not exist.
-
-  ## Examples
-
-      iex> get_collection!(123)
-      %Collection{}
-
-      iex> get_collection!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_collection!(id), do: Repo.get!(Collection, id)
 
   def get_users_collection!(user, id) do
@@ -50,73 +27,71 @@ defmodule GtdToDoApi.Collections do
     |> Repo.get(id)
   end
 
-  @doc """
-  Creates a collection.
-
-  ## Examples
-
-      iex> create_collection(%{field: value})
-      {:ok, %Collection{}}
-
-      iex> create_collection(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_collection(%User{} = owner, attrs \\ %{}) do
     %Collection{}
     |> Collection.changeset(attrs)
-    |> Ecto.Changeset.put_assoc(:owner, owner)
+    |> Changeset.put_assoc(:owner, owner)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a collection.
-
-  ## Examples
-
-      iex> update_collection(collection, %{field: new_value})
-      {:ok, %Collection{}}
-
-      iex> update_collection(collection, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_collection(%Collection{} = collection, attrs) do
     collection
     |> Collection.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a Collection.
-
-  ## Examples
-
-      iex> delete_collection(collection)
-      {:ok, %Collection{}}
-
-      iex> delete_collection(collection)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_collection(%Collection{} = collection) do
     Repo.delete(collection)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking collection changes.
-
-  ## Examples
-
-      iex> change_collection(collection)
-      %Ecto.Changeset{source: %Collection{}}
-
-  """
   def change_collection(%Collection{} = collection) do
     Collection.changeset(collection, %{})
   end
 
   defp users_collection_query(%User{id: owner_id}) do
     from(c in Collection, where: c.owner_id == ^owner_id)
+  end
+
+  # subcollection
+  # //////////////////////////////////////////////////////////////
+
+  alias GtdToDoApi.Collections.Subcollection
+
+  def list_subcollection do
+    Repo.all(Subcollection)
+  end
+
+  def get_subcollection!(id), do: Repo.get!(Subcollection, id)
+
+  def update_subcollection(%Subcollection{} = subcollection, attrs) do
+    subcollection
+    |> Subcollection.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_subcollection(%Subcollection{} = subcollection) do
+    Repo.delete(subcollection)
+  end
+
+  def change_subcollection(%Subcollection{} = subcollection) do
+    Subcollection.changeset(subcollection, %{})
+  end
+
+  def list_subcollections_from_collection(collection) do
+    Repo.all(list_subcollections_query(collection))
+  end
+
+  def get_collection_subcollection(collection, subcollection_id) do
+    Repo.get!(list_subcollections_query(collection), subcollection_id)
+  end
+
+  def create_subcollection(%User{id: owner_id}, collection_id, attrs) do
+    %Subcollection{owner_id: owner_id, collection_id: collection_id}
+    |> Subcollection.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  defp list_subcollections_query(%Collection{} = collection) do
+    from(s in Ecto.assoc(collection, :subcollections))
   end
 end
