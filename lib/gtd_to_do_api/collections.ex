88e06ba2b0f3bc0826bcc378874929f9bd47d612
jacek swoboda <jacswob@gmail.com>
2020-02-04 16:45:55 +0100

    Create functions for lists resource

--- lib/gtd_to_do_api/collections.ex
+++ lib/gtd_to_do_api/collections.ex
@@ -94,4 +94,147 @@ defmodule GtdToDoApi.Collections do
   defp list_subcollections_query(%Collection{} = collection) do
     from(s in Ecto.assoc(collection, :subcollections))
   end
+
+  # list
+  # /////////////////////////////////////////////////////////////
+
+  alias GtdToDoApi.Collections.List
+
+  @doc """
+  Returns the list of lists.
+
+  ## Examples
+
+      iex> list_lists()
+      [%List{}, ...]
+
+  """
+  def list_lists do
+    Repo.all(List)
+  end
+
+  @doc """
+  Returns the list of lists for given collection.
+
+  ## Examples
+
+      iex> list_collection_lists(collection)
+      [%List{collection_id: ^collection.id}, ...]
+
+  """
+  def list_collection_lists(%Collection{} = collection) do
+    Repo.all(list_lists_query(collection))
+  end
+
+  @doc """
+  Gets a single list.
+
+  Raises `Ecto.NoResultsError` if the List does not exist.
+
+  ## Examples
+
+      iex> get_list!(123)
+      %List{}
+
+      iex> get_list!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_list!(id), do: Repo.get!(List, id)
+
+  @doc """
+  Gets a single list for given collection.
+
+  ## Examples
+
+      iex> get_collection_list!(collection, 123)
+      %List{}
+
+      iex> get_collection_list!(collection, 456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_collection_list!(%Collection{} = collection, id),
+    do: Repo.get!(list_lists_query(collection), id)
+
+  @doc """
+  Creates a list.
+
+  ## Examples
+
+      iex> create_list(user, collection, %{field: value})
+      {:ok, %List{}}
+
+      iex> create_list(user, collection, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_list(%User{} = owner, %Collection{} = collection, attrs \\ %{}) do
+    %List{collection_id: collection.id}
+    |> List.changeset(attrs)
+    |> Ecto.Changeset.put_assoc(:owner, owner)
+    |> Ecto.Changeset.put_assoc(:collection, collection)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Updates a list.
+
+  ## Examples
+
+      iex> update_list(list, %{field: new_value})
+      {:ok, %List{}}
+
+      iex> update_list(list, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_list(%List{} = list, attrs) do
+    list
+    |> List.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a List.
+
+  ## Examples
+
+      iex> delete_list(list)
+      {:ok, %List{}}
+
+      iex> delete_list(list)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_list(%List{} = list) do
+    Repo.delete(list)
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking list changes.
+
+  ## Examples
+
+      iex> change_list(list)
+      %Ecto.Changeset{source: %List{}}
+
+  """
+  def change_list(%List{} = list) do
+    List.changeset(list, %{})
+  end
+
+  @doc """
+  Returns query that selects all lists for given collection.
+
+  ## Examples
+
+      iex> list_lists_query(collection)
+      #Ecto.Query<from l0 in GtdToDoApi.Collections.List,
+                    where: l0.collection_id == ^collection.id>
+
+  """
+  def list_lists_query(collection) do
+    from(l in Ecto.assoc(collection, :lists))
+  end
 end
