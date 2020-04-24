5b459e448cde9e019bf2fd8be741a519d153b3e3
jacek swoboda <jacswob@gmail.com>
2020-04-24 14:56:32 +0200

    Repair tests for auth context

--- test/gtd_to_do_api/auth_test.exs
+++ test/gtd_to_do_api/auth_test.exs
@@ -3,6 +3,7 @@ defmodule GtdToDoApi.AuthTest do
 
   alias GtdToDoApi.Auth
   alias GtdToDoApi.Accounts.User
+  alias GtdToDoApi.Guardian
 
   setup %{conn: conn} do
     conn =
@@ -51,7 +52,13 @@ defmodule GtdToDoApi.AuthTest do
     user_password = "some password"
     %User{id: id, email: email} = user_fixture(%{password: user_password})
 
-    assert {:ok, %User{id: ^id}} = Auth.authenticate_user(email, user_password)
+    id = to_string(id)
+
+    assert {:ok, token, refresh_token, expiration} = Auth.authenticate_user(email, user_password)
+    assert {:ok, %{"sub" => ^id}} = Guardian.decode_and_verify(token, %{})
+
+    assert {:ok, %{"sub" => ^id}} =
+             Guardian.decode_and_verify(refresh_token, %{"typ" => "refresh"})
   end
 
   test "authenticate_user/2 returns {:error, message} if email or password are wrong" do
