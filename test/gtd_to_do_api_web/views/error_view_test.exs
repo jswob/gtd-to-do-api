847f5aade7d5a306595f30054a0b078ae20a6744
jswob <jacswob@gmail.com>
2020-07-23 14:13:18 +0200

    Create tests for error_view 422.json

--- test/gtd_to_do_api_web/views/error_view_test.exs
+++ test/gtd_to_do_api_web/views/error_view_test.exs
@@ -1,15 +1,25 @@
 defmodule GtdToDoApiWeb.ErrorViewTest do
   use GtdToDoApiWeb.ConnCase, async: true
 
+  alias GtdToDoApiWeb.ErrorView
+
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
   test "renders 404.json" do
-    assert render(GtdToDoApiWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+    assert render(ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 422.json" do
+    bad_changeset = %Ecto.Changeset{errors: [{:key, {"detail", "dummy"}}]}
+
+    assert render(ErrorView, "422.json", %{changeset: bad_changeset}) == %{
+             errors: [%{key: "detail"}]
+           }
   end
 
   test "renders 500.json" do
-    assert render(GtdToDoApiWeb.ErrorView, "500.json", []) ==
+    assert render(ErrorView, "500.json", []) ==
              %{errors: %{detail: "Internal Server Error"}}
   end
 end
