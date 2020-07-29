fc4f4c10b0090d070395cf3446cbc28e9a9dfc85
jswob <jacswob@gmail.com>
2020-07-29 11:56:30 +0200

    Fix auth pipeline

--- lib/gtd_to_do_api/auth/auth_pipeline.ex
+++ lib/gtd_to_do_api/auth/auth_pipeline.ex
@@ -1,13 +1,13 @@
 defmodule GtdToDoApi.Auth.AuthPipeline do
   use Guardian.Plug.Pipeline,
     otp_app: :gtd_to_do_api,
-    module: Guardian.Token,
-    error_handler: GtdToDoApi
+    module: GtdToDoApi.Auth.Guardian,
+    error_handler: GtdToDoApi.Auth.ErrorHandler
 
   @claims %{}
 
   plug Guardian.Plug.VerifySession, claims: @claims
   plug Guardian.Plug.VerifyHeader, claims: @claims, realm: "Bearer"
   plug Guardian.Plug.EnsureAuthenticated
-  plug Guardian.Plug.LoadResource
+  plug Guardian.Plug.LoadResource, allow_blank: true
 end
